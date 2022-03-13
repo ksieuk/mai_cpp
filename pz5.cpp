dca80a5d7fcc26f5e5180f4dcd81e430123aa1b4
ksieuk <iebebd3@gmail.com>
2022-03-13 18:41:24 +0300

    pz5 array realization

--- pz5.cpp
+++ pz5.cpp
@@ -1,23 +1,34 @@
 #include <iostream>
 #include <cmath>
-
 using namespace std;
 
-int is_prime(int number) {
-    for (int i = 2; i <= sqrt(number); i++) {
-        if (!(number % i)) {
+
+int is_prime(int number, const int prime_numbers[]) {
+    int j = 0;
+    while (prime_numbers[j] != 0) {
+        if (number % prime_numbers[j] == 0) {
             return 0;
         }
+        j++;
     }
     return 1;
 }
 
 int main() {
-    int n, i = 0, num = 2;
+
+
+    int n, i = 1, num = 3;
     cin >> n;
-    while (n != i) {
-        if (is_prime(num)) {
-            cout << num << " ";
+    int prime_numbers[n] = {2};
+
+    if (n < 1) {
+        return 0;
+    }
+    cout << 2 << ' ';
+    while (n > i) {
+        if (is_prime(num, prime_numbers)) {
+            cout << num << ' ';
+            prime_numbers[i] = num;
             i++;
         }
         num++;
