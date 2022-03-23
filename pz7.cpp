e67565c0afe411d05316f9fa790043c2c19fdd42
ksieuk <iebebd3@gmail.com>
2022-03-23 18:35:29 +0300

    fix bugs

--- pz7.cpp
+++ pz7.cpp
@@ -38,29 +38,40 @@ int intersect(double a, double b, double c, double d) {
 }
 
 void check_segments_intersect() {
-    double xa, ya, xb, yb, xc, yc, xd, yd, x, y;
+    double xa, ya, xb, yb, xc, yc, xd, yd, x, y, tmp;
     cin >> xa >> ya;
     cin >> xb >> yb;
     cin >> xc >> yc;
     cin >> xd >> yd;
 
     if (!(intersect(xa, xb, xc, xd)
-    && intersect(ya, yb, yc, yd)
-    && triangle_area(xa, ya, xb, yb, xc, yc) * triangle_area(xa, ya, xb, yb,xd, yd) <= 0
-    && triangle_area(xc, yc, xd, yd, xa, ya) * triangle_area(xc, yc, xd, yd, xb, yb) <= 0))
-    {
+          && intersect(ya, yb, yc, yd)
+          && (triangle_area(xa, ya, xb, yb, xc, yc) * triangle_area(xa, ya, xb, yb, xd, yd) <= 0)
+          && (triangle_area(xc, yc, xd, yd, xa, ya) * triangle_area(xc, yc, xd, yd, xb, yb) <= 0))) {
         cout << "Incorrect";
         return;
     }
-    x = ((xd - xc) * (ya - yc) - (yd - yc) * (xa - xc)) / ((yd - yc) * (xb - xa) - (xd - xc) * (yb - ya));
-    y = ((xb - xa) * (ya - yc) - (yb - ya) * (xa - xc)) / ((yd - yc) * (xb - xa) - (xd - xc) * (yb - ya));
+    //    x = ((xd - xc) * (ya - yc) - (yd - yc) * (xa - xc)) / ((yd - yc) * (xb - xa) - (xd - xc) * (yb - ya));
+    //    y = ((xb - xa) * (ya - yc) - (yb - ya) * (xa - xc)) / ((yd - yc) * (xb - xa) - (xd - xc) * (yb - ya));
+    //    cout << ((xd - xc) * (ya - yc) - (yd - yc) * (xa - xc)) << "     " << ((yd - yc) * (xb - xa) - (xd - xc) * (yb - ya));
+    //    cout << ((xa * yb - xb * ya) * (xd - xc) - (xc * yd - xd * yc) * (xb - xa)) << "    " << ((ya - yb) * (xd - xc) - (yc - yd) * (xb - xa));
+    //    x = ((xa * yb - xb * ya) * (xd - xc) - (xc * yd - xd * yc) * (xb - xa)) /
+    //        ((ya - yb) * (xd - xc) - (yc - yd) * (xb - xa));
+    //    y = ((yc - yd) * x - (xc * yd - xd * yc)) / (xd - xc);
+    //    tmp = ((xc - xa) * (yb - ya) - (xb - xa) * (yc - ya)) / ((xb - xa) * (yd - yc) - (xd - xc) * (yb * (ya)));
+    //    cout << tmp;
+    //    x = xc + tmp * (xd - xc);
+    //    y = yc + tmp * (yd - yc);
+    tmp = ((xa - xb) * (yc - yd) - (ya - yb) * (xc - xd));
+    x = ((xa * yb - ya * xb) * (xc - xd) - (xa - xb) * (xc * yd - yc * xd)) / tmp;
+    y = ((xa * yb - ya * xb) * (yc - yd) - (ya - yb) * (xc * yd - yc * xd)) / tmp;
 
     cout << x << ' ' << y;
 }
 
 int main() {
     // check_triangle_or_rectangle()
-    // check_segments_intersect();
+    check_segments_intersect();
     return 0;
 };
 
